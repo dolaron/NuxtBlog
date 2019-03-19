b40da0161bb8faca9a559c6377516690582aa87b
damian <damianjankowski111@gmail.com>
2019-03-19 15:07:52 +0100

    Init Nuxt Blog

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <section class="featured-posts">
+            <PostPreview v-for="post in posts" :id="post.id" :key="post.id" :is-admin="isAdmin" :thumbnail="post.thumbnail" :title="post.title" :previewText="post.previewText" />
+        </section>
+    </div>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+    components: {
+        PostPreview
+    },
+    props: {
+        isAdmin: {
+            type: Boolean,
+            default: false
+        },
+        posts: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="admin-page">
+        <section class="new-post">
+            <AppButton @click="$router.push('/admin/new-post')">Create Post</AppButton>
+        </section>
+        <section class="existing-posts">
+            <h1>Existing Posts</h1>
+            <PostList :posts="loadedPosts" isAdmin  />
+        </section>
+    </div>
+</template>
+
+<script>
+import PostList from '@/components/Posts/PostList'
+import AppButton from '@/components/UI/AppButton'
+
+export default {
+  layout: 'admin',
+  components: {
+        PostList,
+        AppButton
+  },
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts
+    }
+  },
+  // asyncData(context, callback) {
+  //   setTimeout(() => {
+  //     callback(null, {
+  //       loadedPosts: [{
+  //         id: '1', 
+  //         title: 'First Post',
+  //         previewText: 'This is our first post!',
+  //         thumbnail: 'http://jolttx.com/wp-content/uploads/2019/02/technology-16-9.jpg'
+  //       }, {
+  //         id: '2', 
+  //         title: 'Second Post',
+  //         previewText: 'This is our second post!',
+  //         thumbnail: 'http://jolttx.com/wp-content/uploads/2019/02/technology-16-9.jpg'
+  //       }]
+  //     })
+  //   }, 1500)
+  // }    
+}
+</script>
+
+<style scoped>
+.admin-page {
+  padding: 20px;
+}
+
+.new-post {
+  text-align: center;
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
+}
+
+.existing-posts h1 {
+  text-align: center;
+}
+</style>
+
